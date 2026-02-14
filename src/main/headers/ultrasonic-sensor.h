85fad429f03681aaa97d889c893ac09e4d12f66b
Torin Stanton-Andersson <161447267+Tottocs@users.noreply.github.com>
2026-02-14 00:52:45 +0000

    Update ultrasonic-sensor.h
    Changed to cpp and removed c interface

--- src/main/headers/ultrasonic-sensor.h
+++ src/main/headers/ultrasonic-sensor.h
@@ -1,15 +1,7 @@
 #ifndef ULTRASOUND_SENSOR_H
 #define ULTRASOUND_SENSOR_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-  void US_Pin_Setup(int TrigPin, int EchoPin);
-  int Get_Distance_CM(int TrigPin, int EchoPin);
-
-#ifdef __cplusplus
-}
-#endif
+  void US_Setup(int TrigPin, int EchoPin);
+  unsigned long Get_Distance_CM(void);
 
 #endif

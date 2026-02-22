e28399eef120248cb16ea022c969250bb4675aea
Torin Stanton-Andersson <161447267+Tottocs@users.noreply.github.com>
2026-02-22 23:55:23 +0000

    Update ultrasonic-sensor.h
    Added us sensor class with non blocking code.

--- src/main/headers/ultrasonic-sensor.h
+++ src/main/headers/ultrasonic-sensor.h
@@ -1,7 +1,28 @@
 #ifndef ULTRASOUND_SENSOR_H
 #define ULTRASOUND_SENSOR_H
 
-  void US_Setup(int TrigPin, int EchoPin);
-  unsigned long Get_Distance_CM(void);
-
+class US_Sensor
+{
+private:
+	//Internal trigger pin number
+	int _TrigPin;
+	
+	//static variables
+	static int _EchoPin;
+	static US_Sensor* ActiveSensor;
+	
+	//Ultrasonic sensor variables
+	volatile unsigned long EchoStart = 0;
+	volatile unsigned long EchoDuration = 0;
+	volatile bool EchoComplete = false;
+	
+	//internal functions
+	static void Echo_ISR();
+	void Handle_Interrupt();
+	
+public:
+	US_Sensor(int TrigPin);
+	static bool Setup_Echo_Pin(int EchoPin);
+	unsigned long Get_Distance_CM(void);
+};
 #endif
